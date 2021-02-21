4d8730a7cce0e051275e9e12c4a312292d91b56e
Rob Galvin <rob@robgalvin.co>
2021-02-21 12:34:02 -0500

    updated

--- js/playeah-pops.js
+++ js/playeah-pops.js
@@ -1,3 +1,248 @@
+/* plaYEAH PowerUp For Thinkific v(v1.0.6)
+https://powerups.thinkific.com/pages/playeah
+v1.0.4
+*/
+(function () {
+    var pops = window._playeah.pops;
+    console.log(pops);
+    var _playeah_lottie = null;
+    var _progress_start = -1;
+    var _pops_css= [
+        '#playeah-pops .modal-content{padding:13px 16px;}#playeah-pops .modal-header .close{top:17.5px;right:7px;}#playeah-pops .popup-title{font-size:16px;line-height:1.07;margin-bottom:9px;}#playeah-pops .popup-text{font-size:14px;line-height:1;}#playeah-pops .btn{width:120px;height:36px;background-color:{{color_cta_bg}};color :{{color_cta_text}};font-size:14px;line-height:1;}#playeah-pops .btn:hover{opacity:.85;}#playeah-pops .btn:active{background-color:{{color_cta_bg}};}@media only screen and (max-width:575px){#playeah-pops .modal-content{text-align:center;padding:40px}playeah-pops .modal-header .close{top:5px;right:5px}#playeah-pops .col-md-auto{margin-bottom:30px}#playeah-pops .btn{margin-left:0!important}}'
+    ];
+    var _pops_html = [
+        '<div class="modal position-bottom-center fade m-width-570" id="playeah-pops" playeah-pops-show="bounceIn" playeah-pops-hide="bounceOut"><div class="modal-dialog"><div class="modal-content bg-white"><div class="modal-header"><button type="button" class="close close-dark" data-dismiss="modal"></button></div><div class="modal-body"><div class="col"><div class="row"><div class="col-md-auto col-sm-auto px-0 pop-media">{{media}} </div><div class="col-md-auto col-sm-auto align-self-center"><div class="popup-title pop-heading">{{heading}}</div><p class="popup-text mb-0 pop-subheading">{{subheading}}</p></div><div class="col align-self-center pop-cta-section"><a href="{{cta_link}}" class="btn ml-5 text-white b-radius-25 pop-cta">{{cta_text}}</a></div></div></div></div></div></div></div>'
+    ]
+
+
+    const hide_fs_lottie = function (){
+        console.log("complete"); 
+        $("#playeah-fs").hide();
+        _playeah_lottie.removeEventListener('complete', hide_fs_lottie);
+
+    }
+    const open_url_new_tab = function(url){
+        var win = window.open(url, '_blank');
+        win.focus();        
+    }
+
+    const show_pop = function(index){
+        var pop = pops[index];
+        var pop_type = 0; //TBD replace with cofog from obj
+
+        console.log(pop);
+        
+        //get and replace CSS
+        var css = "<style>"+_pops_css[pop_type]+"</style>";
+        //replace color_cta_bg, color_cta_text
+        css = css.replace(/{{color_cta_bg}}/g,"#FF0000");
+        css = css.replace(/{{color_cta_text}}/g,"#FFFF00");
+        console.log("CSS",css);
+
+        var html = "<style>"+_pops_html[pop_type]+"</style>";
+        html = html.replace(/{{heading}}/g,"Pop Heading");
+        html = html.replace(/{{subheading}}/g,"Pop Sub Heading");
+        html = html.replace(/{{cta_link}}/g,"#somelink");
+        html = html.replace(/{{cta_text}}/g,"Button text");
+        console.log("HTML",html);
+
+        // inject any special CSS for any needed of hiding things
+        // TBD
+
+        //load all into container
+        $('#playeah-pop-container').html(css+html);
+
+        //open pop
+        $('#playeah-pops').modal({show:true})
+
+        //if auto hide
+        //setTimeout(function() {$('#playeah-pops').modal('hide');}, 4000);
+
+    }        
+
+    const inject_fonts= function() {
+           var f = document.getElementsByTagName("script")[0],
+
+           j1 = document.createElement("link");
+           j1.rel = "stylesheet";
+           j1.href = "https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500,700";
+           f.parentNode.insertBefore(j1, f);
+
+           j2 = document.createElement("link");
+           j2.rel = "stylesheet";
+           j2.href = "https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600";
+           f.parentNode.insertBefore(j2, f);
+
+           j3 = document.createElement("link");
+           j3.rel = "stylesheet";
+           j3.href = "https://fonts.googleapis.com/css?family=Playfair+Display:400i,700i";
+           f.parentNode.insertBefore(j3, f);
+
+    }
+
+    const init = function(){
+        $( document ).ready(function() {
+        });
+    }
+
+    const inject_lottie= function(){
+        var f = document.getElementsByTagName("script")[0],
+           j = document.createElement("script");
+           j.async = false;
+           j.src = "https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js";
+           f.parentNode.insertBefore(j, f);
+    }
+
+    const inject_uikit= function(){
+         var f = document.getElementsByTagName("script")[0],
+            j = document.createElement("script");
+            j.async = false;
+            j.src = "https://cdn.jsdelivr.net/npm/uikit@3.6.15/dist/js/uikit.min.js";
+            f.parentNode.insertBefore(j, f);
+
+            j3 = document.createElement("link");
+            j3.rel = "stylesheet";
+            j3.href = "https://cdn.jsdelivr.net/npm/uikit@3.6.15/dist/css/uikit.min.css";
+            //j3.href = "https://getuikit.com/css/theme.css?1395";
+            f.parentNode.insertBefore(j3, f);
+    }
+    const inject_fa= function(){
+           var f = document.getElementsByTagName("script")[0],
+           j = document.createElement("script");
+           j.async = true;
+           j.src = "https://robgalvinco.github.io/fapro/js/all.js";
+           f.parentNode.insertBefore(j, f);
+           
+           j2 = document.createElement("script");
+           j2.async = true;
+           j2.src = "https://robgalvinco.github.io/fapro/js/v4-shims.min.js";
+           f.parentNode.insertBefore(j2, f);
+
+           j3 = document.createElement("link");
+           j3.rel = "stylesheet";
+           j3.href = "https://robgalvinco.github.io/fapro/css/all.css";
+           f.parentNode.insertBefore(j3, f);
+
+    }  
+    const inject_bs= function(){
+        var f = document.getElementsByTagName("script")[0];
+        j = document.createElement("script");
+        j.async = true;
+        j.src = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js";
+        f.parentNode.insertBefore(j, f);
+        
+
+    }  
+    const inject_animate= function(){
+        var f = document.getElementsByTagName("script")[0];
+
+        j3 = document.createElement("link");
+        j3.rel = "stylesheet";
+        j3.href = "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css";
+        f.parentNode.insertBefore(j3, f);
+
+    }  
+
+
+         
+    const inject_html= function(){
+    console.log("injecting html");
+    var html = '<div id="playeah-pop-container"></div>';
+    $("body").append(html);       
+
+    }
+
+
+    const inject_pops_css = function(){
+
+        j3 = document.createElement("link");
+        j3.rel = "stylesheet";
+        j3.href = "https://cdn.jsdelivr.net/gh/robgalvinco/playeah-pops@latest/css/playeah-pops.css";
+        f.parentNode.insertBefore(j3, f);
+
+        //var style = '<style>#playeah-fs{display:none;}.playeah-mini-image{height:64px;width: 64px;}.playeah-nudge-image{height:100px;width: 100px;}.playeah-modal-image{height:200px;width: 200px;margin-left: auto!important;margin-right: auto!important;}</style>';
+        //$("body").prepend(style);       
+    }
+    Array.prototype.contains = function ( needle ) {
+       for (i in this) {
+           if (this[i] == needle) return true;
+       }
+       return false;
+    }   
+    const find_and_play = function(progress, courseid){
+        console.log(progress,courseid);
+        var __allcourses_index = pops.findIndex(function(pop, index) {
+        if(pop.id == "all" && pop.progress<=progress  && !pop.played)
+            return true;
+        });     
+        console.log("all index: "+__allcourses_index);  
+        var __courses_index = pops.findIndex(function(pop, index) {
+            if(pop.courses.contains(courseid)  && pop.progress<=progress  && !pop.played)
+                return true;
+        }); 
+        console.log("course index: "+__courses_index);
+        if(__courses_index != -1){
+                show_pop(__courses_index);
+                //make sure the "all" one with same progress does not play
+                __allcourses_index = pops.findIndex(function(pop, index) {
+                    if(pop.id == "all" && pop.progress==progress  && !pop.played)
+                        return true;
+                }); 
+                if(__allcourses_index != -1){
+                    pops[__allcourses_index].played=true;
+                }
+        
+        }  else {
+            if(__allcourses_index != -1){
+                show_pop(__allcourses_index)
+            }
+        }
+    }
+    $( document ).ready(function() {
+        if(typeof(CoursePlayerV2) !== 'undefined') {
+            inject_fonts();
+            inject_fa();
+            inject_animate();
+            inject_pops_css();
+            inject_lottie();
+            inject_bs();
+            inject_html();
+        
+        
+            CoursePlayerV2.on('hooks:contentDidChange', function(data) {
+                console.log(data);
+                if(_progress_start == -1){
+                    console.log("Setting default");
+                    _progress_start = data.enrollment.percentage_completed*100;
+                    console.log("start: "+_progress_start)
+                    //mark pops played if progress is >0
+                    for (i = 0; i < pops.length; i++) {
+                        if(pops[i].progress <= _progress_start){
+                            pops[i].played=true;
+                        }
+                    }                
+                }
+                
+            });
+
+            CoursePlayerV2.on('hooks:contentWasCompleted', function(data) {
+                console.log(data);
+                var progress = data.enrollment.percentage_completed*100;
+                console.log(progress)
+                find_and_play(progress,data.course.id)
+            });
+             
+        }
+
+    });
+
+        
+        
+    
+}());
+
+
+
 
 /* 
 ouibounce.js
@@ -689,5 +934,8 @@ jQuery(function($) {
         Cookies.remove('popup', { path: '/' }); // removed cookie!
     });
 });
+
+
+
 
  
--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 # How it works
 * always include `/css/playeah-pops.css` and `/js/playeah-pops.js`
+* some code to determine if pop was triggered (progress, lesson start, etc)
+* pop config has attribute that says the pop type (index into pops array)
 * css and html strings will be stored in code in arrays (minified)
 * replace placeholder `{{ variable }}` text with pop settings for both #.css and html.css
 * inject any needed style for hiding ex: not using images, or cta
@@ -9,8 +11,8 @@
 * insert css and html into the document
 * load pop / determine if need to autohide after delay
 
-#css
-* pop-img
+#css (add this to snippets)
+* pop-media
 * pop-heading
 * pop-subheading
 * pop-cta-section
@@ -19,8 +21,7 @@
 #{{names}} will be used to auto replace content
 
 ## html
-* {{lottie}} - will be area of lottie player - need to remove it using imag
-* {{img}} - will be image
+* {{media}} - will be area of media - img,lottie,video
 * {{heading}}
 * {{subheading}}
 * {{cta_link}}
@@ -29,4 +30,9 @@
 #css
 * {{color_cta_bg}}
 * {{color_cta_text}}
+
+#Minify command -option -m
+
+
+
 
